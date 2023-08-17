ad7e680d2b3c5910585967102a364d8565ef1a4c
rajatpzade <rajat@gmail.com>
2023-08-17 15:42:40 +0530

    --- Auto Git Commit ---

--- jenkins.sh
+++ jenkins.sh
@@ -2,18 +2,9 @@
 
 #!/bin/bash 
 
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
-  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
 
-  echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
-  /etc/apt/sources.list.d/jenkins.list > /dev/null
+    
 
-  apt-get update
-
-  apt install openjdk-11-jre
-
-  apt-get update
-
-  
 
