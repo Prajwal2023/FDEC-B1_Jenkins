35ee021e1733ab1cfbf3ef3512107706a8181733
rajatpzade <rajat@gmail.com>
2023-08-18 12:52:30 +0530

    --- Auto Git Commit ---

--- scripted.groovy
+++ scripted.groovy
@@ -6,9 +6,9 @@ node {
         echo "Build is available"
     }
     stage = ('Test') {
-        echo "Test is successful passing tests"
+        echo "Code is successful passing tests"
     }
     stage = ('Deploy') {
-        echo "Build is available"
+        echo "Con"
     }
 }
