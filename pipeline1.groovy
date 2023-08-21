2437863c23c9b30b9db6e562d2c748ed25c456dd
rajatpzade <rajat@gmail.com>
2023-08-21 12:12:46 +0530

    --- Auto Git Commit ---

--- pipeline1.groovy
+++ pipeline1.groovy
@@ -3,7 +3,7 @@ pipeline {
     stages {
         stage('Pull') { 
             steps {
-                
+                git branch: 'main', url: 'https://github.com/rajatpzade/FDEC-B1_Jenkins.git'
             }
         }
         stage('Build') { 
