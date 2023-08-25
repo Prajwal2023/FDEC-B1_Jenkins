9c1c6aa86f3f9958254eb4a2a2c0768d8ba13431
rajatpzade <rajat@gmail.com>
2023-08-25 15:28:29 +0530

    --- Auto Git Commit ---

--- pipeline1.groovy
+++ pipeline1.groovy
@@ -3,7 +3,7 @@ pipeline {
     stages {
         stage('Pull') { 
             steps {
-                git branch: 'main', url: 'https://github.com/rajatpzade/studentapp.ui.git'
+                git branch: 'master', url: 'https://github.com/rajatpzade/studentapp.ui.git'
             }
         }
         stage('Build') { 
